adf1d23e46fc69275d4d3946fc6535e77b3d3f96
zhangqianli <zhangqianli414@163.com>
2018-04-04 15:46:22 +0800

    改动Animal类，添加字段和方法

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,20 @@
 public class Animal {
+    private String name;
+    private double height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
